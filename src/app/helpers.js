0a7ce2ffea28de6bf48d479cf2afaaf2528e17fe
martins021 <martins.miesnieks@gmail.com>
2023-11-28 13:03:58 +0200

    adjacency list holds information about edge weights
    Weights are calculated from the elevations of starting node and end node. Weights are in the range of 1 to 400 inclusive

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -1,11 +1,57 @@
-const createAdjacencyList = (data, colCount, rowCount) => {
+const getWeight = (nodeTo, nodeFrom) => {
+  if(nodeFrom > nodeTo){ // going from higher elevation to lower
+    return 200 - Math.abs(nodeTo - nodeFrom);
+  } else if (nodeFrom < nodeTo) { // going from lower elevation to higher
+    return 200 + Math.abs(nodeTo - nodeFrom) + 1;
+  } else {
+    return 200; // no elevation change
+  }
+}
+
+const createAdjacencyList = (data, colCount, rowCount, weighted = false) => {
   const adjList = data.map((node, i) => {
     const neighbors = []; // holds indices of neighboring nodes
     if(node.state === "wall") return neighbors; // wall nodes have no neighbors
-    if(i > colCount - 1) neighbors.push(i - colCount); // top neighbor
-    if(i % colCount !== 0) neighbors.push(i - 1); // left neighbor
-    if(i % colCount !== colCount - 1) neighbors.push(i + 1); // right neighbor
-    if(i < colCount * (rowCount - 1)) neighbors.push(i + colCount); // bottom neighbor
+    if(i > colCount - 1)  { // top neighbor
+      if(weighted){
+        neighbors.push({
+          nodeId: i - colCount,
+          weight: getWeight(data[i - colCount].elev, node.elev)
+        })
+      } else {
+        neighbors.push(i - colCount);
+      }
+    } 
+    if(i % colCount !== 0) {  // left neighbor
+      if(weighted){
+        neighbors.push({
+          nodeId: i - 1,
+          weight: getWeight(data[i - 1].elev, node.elev)
+        })
+      } else {
+        neighbors.push(i - 1);
+      }
+    }
+    if(i % colCount !== colCount - 1) { // right neighbor
+      if(weighted){
+        neighbors.push({
+          nodeId: i + 1,
+          weight: getWeight(data[i + 1].elev, node.elev)
+        })
+      } else {
+        neighbors.push(i + 1);
+      }
+    } 
+    if(i < colCount * (rowCount - 1)) { // bottom neighbor
+      if(weighted){
+        neighbors.push({
+          nodeId: i + colCount,
+          weight: getWeight(data[i + colCount].elev, node.elev)
+        })
+      } else {
+        neighbors.push(i + colCount);
+      }
+    } 
     return neighbors;
   });
   return adjList;
