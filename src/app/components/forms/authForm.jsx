51ed5da0f1ca0a922516c59dd09aa1a0d90b5f48
martins021 <martins.miesnieks@gmail.com>
2023-12-05 10:54:45 +0200

    styled authorization form

--- src/app/components/forms/authForm.jsx
+++ src/app/components/forms/authForm.jsx
@@ -3,34 +3,38 @@ import React from "react";
 import styles from "../../styles/auth.module.css"
 import { useForm } from "react-hook-form"
 import { usePathname } from 'next/navigation'
+import { Input, Box } from '@chakra-ui/react'
+
 
 const AuthForm = ({ submitText, onSubmit, referrer = "register" }) => {
   const { register, handleSubmit } = useForm();
   const pathname = usePathname();
 
   return(
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.authForm}>
-      {!pathname.includes("login") && 
-      <>
+    <Box className={styles.authForm} borderColor={"gray"} borderWidth='1px' borderRadius='lg' p={8}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.authForm}>
+        {!pathname.includes("login") && 
+        <>
+          <label>
+            Username:
+          </label>
+          <Input { ...register("username") } />
+        </>
+        }
+
         <label>
-          UserName:
+          E-mail:
         </label>
-        <input { ...register("username") } />
-      </>
-      }
+        <Input type="email" { ...register("email") } />
 
-      <label>
-        E-mail:
-      </label>
-      <input { ...register("email") } />
+        <label>
+          Password:
+        </label>
+        <Input type="password" { ...register("password") } />
 
-      <label>
-        Password:
-      </label>
-      <input { ...register("password") } />
-
-      <input type="submit" value={submitText} />
-    </form>
+        <Input type="submit" value={submitText} style={{ marginTop: 20 }} className={styles.submitBtn} />
+      </form>
+    </Box>
   )
 }
 
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from 'next/navigation'
 import styles from "../styles/auth.module.css"
 import AuthForm from "../components/forms/authForm";
