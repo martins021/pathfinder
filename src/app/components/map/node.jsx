ee04917f96edee9ea0be2feabce3a5f915c73a26
martins021 <martins.miesnieks@gmail.com>
2025-10-19 15:13:33 +0300

    better cell class handling

--- src/app/components/map/node.jsx
+++ src/app/components/map/node.jsx
@@ -3,39 +3,32 @@ import React, { memo } from "react";
 import styles from "../../styles/map.module.css";
 import { elevationColors } from "@/lib/configs";
 
-const createClassname = (cellState, prevCellState) => {
-  let string = `${styles["cell"]}`;
-  if (cellState === "start"){
-    string += ` ${styles[prevCellState]} ${styles["start"]}`
-  } else if (cellState === "target"){
-    string += ` ${styles[prevCellState]} ${styles["target"]}`
-  } else {
-    string += ` ${styles[cellState]}`
-  }
-  return string;
-}
+const baseColors = new Map([
+  ["wall", "rgb(53, 53, 53)"],
+  ["start", "rgb(60, 110, 113)"],
+  ["target", "rgb(164, 22, 26)"],
+])
 
 const createAnimationTerrainColor = (color) => {
   return color.replace('rgb', 'rgba').replace(')', `, 0.5)`);
 }
 
-const Node = ({ i, prevCellState, cellState, delay, onClick, onMouseLeave, elevation }) => {
-  delay = delay ? delay : i / 1000;
+const Node = ({ i, prevCellState, cellState, onClick, onMouseLeave, elevation }) => {
+  const bckGroundColor = baseColors.get(cellState) || elevationColors[elevation + 100];
   return (
     <div
-      className={createClassname(cellState, prevCellState)}
+      className={`${styles["cell"]} ${styles[cellState]}`}
       onClick={onClick}
       onMouseLeave={onMouseLeave}
       style={{ 
-        "--delay": `${delay}s`, 
+        backgroundColor: "rgb(53, 53, 53)", // pre-animation color
+        "--delay": `${i / 1000}s`, 
+        "--baseBackgroundColor": bckGroundColor,
         "--animationFinalBackgroundColor": createAnimationTerrainColor(elevationColors[elevation + 100]),
         "--animationInitialBackgroundColor": elevationColors[elevation + 100],
-        backgroundColor: "rgb(53, 53, 53)",
-        "--baseBackgroundColor": cellState === "wall" ? "rgb(53, 53, 53)" : elevationColors[elevation + 100],
-        fontSize: 12
       }}
       data-testid="map-node"
-    >{i}</div>
+    ></div>
   )
 };
 
