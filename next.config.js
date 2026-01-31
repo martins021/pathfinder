a0aff4746e9ece1cf0626c0d9dff3264b60fe197
martian <martins.miesnieks@gmail.com>
2026-01-31 17:25:32 +0200

    dockerize app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "standalone",
+    turbopack: {
+        resolveAlias: {
+            "@": "./src",
+        },
+    }
+}
 
 module.exports = nextConfig
